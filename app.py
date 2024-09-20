9c6f02d6d1be431d3c467b8dbcc6735af85cda2c
Giorgia Iacobellis <giorgiaiacobellis2000@gmail.com>
2024-09-20 17:42:30 +0200

    pipeline evaluator 2

--- app.py
+++ app.py
@@ -26,14 +26,15 @@ def main():
   # embedding model
   embed_model = HuggingFaceEmbeddings(model_name='sentence-transformers/all-mpnet-base-v2')
 
-  llm = HuggingFaceEndpoint(
-  repo_id="mistralai/Mistral-Nemo-Instruct-2407",
-  task="text-generation",
-  max_new_tokens=512,
-  do_sample=False,
-  repetition_penalty=1.03,
+  pipe2 = pipeline(
+      model=AutoModelForCausalLM.from_pretrained("mistralai/Mistral-Nemo-Instruct-2407"),
+      tokenizer=AutoTokenizer.from_pretrained("mistralai/Mistral-Nemo-Instruct-2407"),
+      return_full_text=True,  # langchain expects the full text
+      task='text-generation',
+      #temperature=0.5,
+      repetition_penalty=1.03,  # without this output begins repeating
+      max_new_tokens = 512
   )
-
 
   pipe = pipeline(
       model=AutoModelForCausalLM.from_pretrained("HuggingFaceH4/zephyr-7b-beta"),
@@ -45,11 +46,13 @@ def main():
       max_new_tokens = 512
   )
 
-  evaluator = HuggingFacePipeline(pipeline=pipe)
+  evaluator1 = HuggingFacePipeline(pipeline=pipe)
+  
+  evaluator2 = HuggingFacePipeline(pipeline=pipe2)
 
   #utils.chat_with_chatbot(data.config1, "session_1")
-  utils.evaluate_model(data.config1, evaluator, embed_model, data.questions, data.answers)
-  utils.evaluate_model(data.config1, llm, embed_model, data.questions, data.answers) 
+  utils.evaluate_model(data.config1, evaluator1, embed_model, data.questions, data.answers)
+  utils.evaluate_model(data.config1, evaluator2, embed_model, data.questions, data.answers) 
 
 
 if __name__ == "__main__":
