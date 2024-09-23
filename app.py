9e846329ac21ba6d68edaa813cd167064323c028
Federico Torrielli <evilscript@protonmail.com>
2024-09-23 14:35:51 +0200

    mammt

--- app.py
+++ app.py
@@ -1,57 +1,37 @@
-#from ragas.embeddings import HuggingfaceEmbeddings
-#from ragas.llms import LangchainLLMWrapper
 from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
-from langchain_huggingface import HuggingFaceEmbeddings, HuggingFacePipeline
+from langchain_huggingface import HuggingFacePipeline
 import utils
 import data
 import os
 
 
-
-os.environ['OPENAI_API_KEY'] = 'sk-rf-yLyTntiSYVkhQm8O5bgiGQn1GAYwlPngB80vlNsT3BlbkFJtntowM_ykl6TVjFdZalhu6MuYHeBdSMh1OJmtqbH4A'
-os.environ["HUGGINGFACE_ACCESS_TOKEN"] =  "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" #"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"
+os.environ["OPENAI_API_KEY"] = (
+    "sk-rf-yLyTntiSYVkhQm8O5bgiGQn1GAYwlPngB80vlNsT3BlbkFJtntowM_ykl6TVjFdZalhu6MuYHeBdSMh1OJmtqbH4A"
+)
+os.environ["HUGGINGFACE_ACCESS_TOKEN"] = (
+    "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"  # "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"
+)
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
-
-#app.reset()
-# Carica i dati in batch da cleaned_data.json
-#load_data_in_batches("cleaned_data.json", app)
-#print("Caricamento dati completato!")
 
 
 def main():
-  
-  # embedding model
-  embed_model = HuggingFaceEmbeddings(model_name='sentence-transformers/all-mpnet-base-v2')
 
-  pipe = pipeline(
-      model=AutoModelForCausalLM.from_pretrained("arcee-ai/Llama-3.1-SuperNova-Lite"),
-      tokenizer=AutoTokenizer.from_pretrained("arcee-ai/Llama-3.1-SuperNova-Lite"),
-      return_full_text=True,  # langchain expects the full text
-      task='text-generation',
-      temperature=0.5,
-      repetition_penalty=1.1,  # without this output begins repeating
-      max_new_tokens = 512,
-      device= "cuda:1"
-      
-  )
+    pipe = pipeline(
+        model=AutoModelForCausalLM.from_pretrained("arcee-ai/Llama-3.1-SuperNova-Lite"),
+        tokenizer=AutoTokenizer.from_pretrained("arcee-ai/Llama-3.1-SuperNova-Lite"),
+        return_full_text=True,  # langchain expects the full text
+        task="text-generation",
+        temperature=0.5,
+        repetition_penalty=1.1,  # without this output begins repeating
+        max_new_tokens=512,
+        device=0,
+    )
 
-  pipe2 = pipeline(
-      model=AutoModelForCausalLM.from_pretrained("mistralai/Mistral-Nemo-Instruct-2407"),
-      tokenizer=AutoTokenizer.from_pretrained("mistralai/Mistral-Nemo-Instruct-2407"),
-      return_full_text=True,  # langchain expects the full text
-      task='text-generation',
-      #temperature=0.5,
-      repetition_penalty=1.03,  # without this output begins repeating
-      max_new_tokens = 512,
-      device= "cuda:1"
-  )
+    evaluator1 = HuggingFacePipeline(pipeline=pipe)
 
-  evaluator1 = HuggingFacePipeline(pipeline=pipe)
-  #evaluator2 = HuggingFacePipeline(pipeline=pipe2)
-
-  #utils.chat_with_chatbot(data.config1, "session_1")
-  utils.evaluate_model(data.config1, evaluator1, embed_model, data.questions, data.answers)
-  #utils.evaluate_model(data.config1, evaluator2, embed_model, data.questions, data.answers) 
+    utils.evaluate_model(
+        data.config, evaluator1, data.questions, data.answers
+    )
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-import os
 from embedchain import App
 import json
 from datasets import Dataset
@@ -7,15 +6,13 @@ from ragas.metrics import (
     faithfulness,
     answer_relevancy,
     answer_correctness,
-    context_recall,
-    context_precision,
-    summarization_score
 )
 
 # Dimensione del batch per il caricamento dei dati
-BATCH_SIZE = 100 
+BATCH_SIZE = 100
 
-#funzione che adatta il prompt in base al modello
+
+# funzione che adatta il prompt in base al modello
 def get_modified_prompt(system: str, model_name: str) -> str:
     if "gemma" in model_name.lower() and "-it" in model_name.lower():
         return f"<bos><start_of_turn>user\n{system}nContext information:\n----------------------\n$context\n----------------------\n$history\n $query<end_of_turn>\n<start_of_turn>model\n Answer: "
@@ -24,7 +21,7 @@ def get_modified_prompt(system: str, model_name: str) -> str:
         return f"<|system|>{system}nContext information:\n----------------------\n$context\n----------------------\n$history\n</s><|user|>\n\nQuery: $query</s><|assistant|>\n Answer: "
 
     elif "meta" in model_name.lower():
-        return (f"<|begin_of_text|><|start_header_id|>system<|end_header_id|>{system}nContext information:\n----------------------\n$context\n----------------------\n$history\n<|eot_id|><|start_header_id|>user<|end_header_id|>Query: $query<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n Answer: ")
+        return f"<|begin_of_text|><|start_header_id|>system<|end_header_id|>{system}nContext information:\n----------------------\n$context\n----------------------\n$history\n<|eot_id|><|start_header_id|>user<|end_header_id|>Query: $query<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n Answer: "
 
     elif "c4ai" in model_name.lower():
         return f"<BOS_TOKEN><|START_OF_TURN_TOKEN|><|USER_TOKEN|>{system}nContext information:\n----------------------\n$context\n----------------------\n$history\n$query<|END_OF_TURN_TOKEN|><|START_OF_TURN_TOKEN|><|CHATBOT_TOKEN|> Answer: "
@@ -35,67 +32,69 @@ def get_modified_prompt(system: str, model_name: str) -> str:
     return f"[INST]{system}nContext information:\n----------------------\n$context\n----------------------\n$history\n $query[/INST]\n Answer:"
 
 
-#funzione per caricare i dati in batch nel vector db (da non usare)
+# funzione per caricare i dati in batch nel vector db (da non usare)
 def load_data_in_batches(filename, app):
     """Carica i dati dal file JSON in batch e li aggiunge a Embedchain."""
 
-    with open(filename, 'r', encoding='utf-8') as f:
+    with open(filename, "r", encoding="utf-8") as f:
         data = json.load(f)
     print(len(data))
     for i in range(0, len(data), BATCH_SIZE):
-        batch = data[i : i + BATCH_SIZE]       
-        
+        batch = data[i : i + BATCH_SIZE]
+
         # Formatta i dati per Embedchain
         formatted_batch = [
-            {"text": item["text"]} for item in batch  # Estrai il campo "text"
+            {"text": item["text"]}
+            for item in batch  # Estrai il campo "text"
         ]
         formatted_batch = json.dumps(formatted_batch)
         app.add(formatted_batch)  # Aggiungi il batch a Embedchain
         print(f"Aggiunto batch {i // BATCH_SIZE + 1} di {len(data) // BATCH_SIZE}")
 
 
-
 # funzione che genera il dataset di [query, answer, context, ground_truth]
 def generate_responses(app, test_questions, test_answers):
-  answers = []
-  contexts = []
+    answers = []
+    contexts = []
 
-  for q in test_questions:
-    #print(q)
-    answer, context = app.query(q, citations = True)
-    #print("Answer:", answer[answer.find("Answer:") + len("Answer"):].strip())
-    answers.append(answer[answer.find("Answer:") + len("Answer"):].strip())
-    contexts.append([tupla[0] for tupla in context])
+    for q in test_questions:
+        # print(q)
+        answer, context = app.query(q, citations=True)
+        # print("Answer:", answer[answer.find("Answer:") + len("Answer"):].strip())
+        answers.append(answer[answer.find("Answer:") + len("Answer") :].strip())
+        contexts.append([tupla[0] for tupla in context])
 
-
-  dataset_dict = {
+    dataset_dict = {
         "question": test_questions,
         "answer": answers,
         "contexts": contexts,
-  }
-  if test_answers is not None:
-    dataset_dict["ground_truth"] = test_answers
-  ds = Dataset.from_dict(dataset_dict)
-  return ds
+    }
+    if test_answers is not None:
+        dataset_dict["ground_truth"] = test_answers
+    ds = Dataset.from_dict(dataset_dict)
+    return ds
 
 
-#funzione per testare il modello in una conversazione
+# funzione per testare il modello in una conversazione
 def chat_with_chatbot(config, session):
-
-    app = App.from_config(config = config) #create app from config
+    app = App.from_config(config=config)  # create app from config
     filename = "chatbot_responses.json"
-    app.delete_all_chat_history(app.id)#clean della chat history
+    app.delete_all_chat_history(app.id)  # clean della chat history
 
     while True:
         try:
-            input_query = input("\nCosa vuoi chiedere al chatbot? (Digita 'esci' per uscire) ")
+            input_query = input(
+                "\nCosa vuoi chiedere al chatbot? (Digita 'esci' per uscire) "
+            )
 
             # Controlla se l'utente vuole uscire
-            if input_query.lower() == 'esci':
+            if input_query.lower() == "esci":
                 break
 
-            answer = app.chat(input_query, session_id=session, citations=False)  # Assumiamo che 'app' sia definito correttamente
-            reduced = answer[answer.find("Answer:") + len("Answer:"):].strip()
+            answer = app.chat(
+                input_query, session_id=session, citations=False
+            )  # Assumiamo che 'app' sia definito correttamente
+            reduced = answer[answer.find("Answer:") + len("Answer:") :].strip()
             print(f"{reduced}\n\n")
 
             # Crea un dizionario per memorizzare i dati dell'interazione corrente
@@ -104,7 +103,7 @@ def chat_with_chatbot(config, session):
                     {
                         "config_data": app.llm.config.serialize(),
                         "query": input_query,
-                        "response": answer
+                        "response": answer,
                     }
                 ]
             }
@@ -123,37 +122,38 @@ def chat_with_chatbot(config, session):
             with open(filename, "w") as f:
                 json.dump(existing_data, f, indent=4)
 
-            #print("Dati dell'interazione salvati correttamente in", filename)
+            # print("Dati dell'interazione salvati correttamente in", filename)
 
         except EOFError:
             print("Fine dell'input. Arrivederci!")
             break
 
 
-#funzione per valutare il modello 
-def evaluate_model(config, evaluator, embedding, test_questions, test_answers, filename="evaluation_results.json"):
-
-    app = App.from_config(config = config) #create app from config
+# funzione per valutare il modello
+def evaluate_model(
+    config, evaluator, test_questions, test_answers, filename="evaluation_results.json"
+):
+    app = App.from_config(config=config)  # create app from config
     llm_config = app.llm.config.serialize()
 
-    app.delete_all_chat_history(app.id) #clean della chat history
-    
-    ds = generate_responses(app, test_questions, test_answers) #genera le risposte del modello
+    app.delete_all_chat_history(app.id)  # clean della chat history
 
-    try : 
+    ds = generate_responses(
+        app, test_questions, test_answers
+    )  # genera le risposte del modello
+
+    try:
         # Valuta il modello
         results = evaluate(
             llm=evaluator,
             dataset=ds,
-            embeddings=embedding,
             metrics=[
                 faithfulness,
                 answer_relevancy,
                 answer_correctness,
-                #context_recall,
-                #context_precision
-                
-            ]
+                # context_recall,
+                # context_precision
+            ],
         )
         print(results)
     except Exception as e:
@@ -161,10 +161,7 @@ def evaluate_model(config, evaluator, embedding, test_questions, test_answers, f
         results = None
 
     # Salva i risultati in un file
-    save_results = {
-        "model_info": llm_config,
-        "evaluation_results": results  
-    }
+    save_results = {"model_info": llm_config, "evaluation_results": results}
 
     try:
         with open(filename, "r") as f:
@@ -176,9 +173,8 @@ def evaluate_model(config, evaluator, embedding, test_questions, test_answers, f
     existing_result.append(save_results)
 
     with open(filename, "w") as f:
-        json.dump(existing_result, f, indent=4)  
+        json.dump(existing_result, f, indent=4)
 
     print(f"Results saved to {filename}")
 
     return results
-
