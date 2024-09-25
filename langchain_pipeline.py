ca9aff263634ea6241c94e30bdb28af18ec36ef2
Giorgia Iacobellis <giorgiaiacobellis2000@gmail.com>
2024-09-25 12:36:21 +0200

    aggiunto context al prompt

--- langchain_pipeline.py
+++ langchain_pipeline.py
@@ -6,6 +6,8 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_chroma import Chroma
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.docstore.document import Document
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
 
 import os
 import json
@@ -18,6 +20,11 @@ os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_API_KEY"] = "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"
 
+#VARIABILI DI CONTROLLO
+SPLIT = 1  # vale zero se lo split è da eseguire altimenti 1
+VECTORDB = 1 #vale 0 se il vectordb è da costruire altrimenti 1
+
+
 def main():
     # Caricamento del modello LLM
     print("ehi fratello, sto caricando il modello!")
@@ -29,41 +36,48 @@ def main():
         stream=True
     )
 
-    print("yo gang, sto caricando i dati!")
-    # Caricamento dei dati
-    with open("cleaned_data.json", "r") as f: # Caricamento dei dati dal file JSON
-        json_data = json.load(f)
 
-    # Conversione dei dati in documenti Langchain
-    documents = [
-        Document(page_content=item["text"], metadata={"source": item["page_name"]})
-        for item in json_data
-    ]
+    if SPLIT == 0:
+        print("yo gang, sto caricando i dati!")
+        # Caricamento dei dati
+        with open("cleaned_data.json", "r") as f: # Caricamento dei dati dal file JSON
+            json_data = json.load(f)
 
-    print("e diamogliela na splittata a sti docssss")
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    splits = text_splitter.split_documents(documents)
+        # Conversione dei dati in documenti Langchain
+        documents = [
+            Document(page_content=item["text"], metadata={"source": item["page_name"]})
+            for item in json_data
+        ]
 
+        print("e diamogliela na splittata a sti docssss")
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        splits = text_splitter.split_documents(documents)
 
-    print("lo facciamo o non lo facciamo sto embedder?")
     #Vector DB
+    print("lo facciamo o non lo facciamo sto embedder?")
     embedder = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-mpnet-base-v2",
         model_kwargs={"trust_remote_code": True, "device": 0},
     )
 
     print("grande fra, hai raggiunto il livello VectorDB, ecco a te il tuo calippo!")
-    vectordb = Chroma.from_documents(documents=splits, 
-                            collection_name="turism_collection",
-                            embedding=embedder,
-                            persist_directory="./chroma_langchain_db")
+    vectordb = Chroma(collection_name="turism_collection",
+                      embedding=embedder,
+                      persist_directory="./chroma_langchain_db")
+    
+    if VECTORDB == 0:
+        vectordb.from_documents(documents=splits, 
+                                collection_name="turism_collection",
+                                embedding=embedder,
+                                persist_directory="./chroma_langchain_db")
+    
     retriever = vectordb.as_retriever()
 
 
-    prompt = ChatPromptTemplate.from_messages(
+    prompt = ChatPromptTemplate(
         [
             ("system", utils.get_modified_prompt(
-            "Sei un assistente turistico specializzato nel Piemonte. il tuo obiettivo è fornire informazioni accurate, utili e interessanti ai turisti che desiderano visitare il Piemonte. rispondi a domande su attrazioni turistiche, eventi, itinerari, cucina tipica, trasporti, alloggi e altre informazioni utili per i turisti. Sii preparato a rispondere a domande aperte, richieste di consigli e suggerimenti personalizzati in base agli interessi e alle esigenze dei turisti. Usa un tono amichevole, accogliente e professionale. sii entusiasta di condividere le bellezze e le peculiarità del Piemonte. Adatta il tuo stile di comunicazione al pubblico di riferimento che può includere famiglie, coppie, viaggiatori solitari, appassionati di enogastronomia, amanti della natura, ecc. Utilizza le informazioni estratte dai siti web dei comuni del Piemonte e altre fonti affidabili per fornire risposte accurate e aggiornate. Se non sei sicuro di una risposta, ammettilo onestamente e suggerisci alte fonti di informazione o modalità di contatti  per ottenere ulteriori dettaglio.",
+            "Sei un assistente turistico specializzato nel Piemonte. il tuo obiettivo è fornire informazioni accurate, utili e interessanti ai turisti che desiderano visitare il Piemonte. rispondi a domande su attrazioni turistiche, eventi, itinerari, cucina tipica, trasporti, alloggi e altre informazioni utili per i turisti. Sii preparato a rispondere a domande aperte, richieste di consigli e suggerimenti personalizzati in base agli interessi e alle esigenze dei turisti. Usa un tono amichevole, accogliente e professionale. sii entusiasta di condividere le bellezze e le peculiarità del Piemonte. Adatta il tuo stile di comunicazione al pubblico di riferimento che può includere famiglie, coppie, viaggiatori solitari, appassionati di enogastronomia, amanti della natura, ecc. Utilizza le informazioni estratte dai siti web dei comuni del Piemonte e altre fonti affidabili per fornire risposte accurate e aggiornate. Se non sei sicuro di una risposta, ammettilo onestamente e suggerisci alte fonti di informazione o modalità di contatti per ottenere ulteriori dettaglio.",
             "HuggingFaceH4/zephyr-7b-beta",
         ),),
             ("human", "{input}"),
@@ -71,6 +85,8 @@ def main():
     )
 
     print("chattiamo va! Boia faus!")
+
+
     question_answer_chain = create_stuff_documents_chain(llm, prompt)
     rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 
