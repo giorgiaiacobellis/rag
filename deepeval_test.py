89e6c67f68e4d4b0e3bdc91a326050831d3c4114
Giorgia Iacobellis <giorgiaiacobellis2000@gmail.com>
2024-10-06 14:38:54 +0200

    prova con deep_eval

---
+++ deepeval_test.py
@@ -0,0 +1,65 @@
+from deepeval.test_case import LLMTestCase
+from deepeval.metrics import AnswerRelevancyMetric
+from deepeval import evaluate
+import json
+import transformers
+from deepeval.models.base_model import DeepEvalBaseLLM
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+def costruisci_testcases(file):
+    test_cases = []
+    with open(file, 'r') as f:
+        data = json.load(f)
+
+    for question, answer, ground, context in zip(data['data']['question'], data['data']['answer'], data['data']['contexts'], data['data']['ground_truth']):
+        test_case = LLMTestCase(input=question, actual_output=answer, expected_output = ground, retrieval_context=context)
+        test_cases.append(test_case)
+
+    return test_cases
+
+
+class Zephyr(DeepEvalBaseLLM):
+    def __init__(
+        self,
+        model,
+        tokenizer
+    ):
+        self.model = AutoModelForCausalLM.from_pretrained("HuggingFaceH4/zephyr-7b-beta")
+        self.tokenizer = AutoTokenizer.from_pretrained("HuggingFaceH4/zephyr-7b-beta1")
+
+    def load_model(self):
+        return self.model
+
+    def generate(self, prompt: str) -> str:
+        model = self.load_model()
+
+        pipeline = transformers.pipeline(
+            "text-generation",
+            model=model,
+            tokenizer=self.tokenizer,
+            use_cache=True,
+            device="cuda:0",
+            max_length=2500,
+            do_sample=True,
+            top_k=5,
+            num_return_sequences=1,
+            eos_token_id=self.tokenizer.eos_token_id,
+            pad_token_id=self.tokenizer.eos_token_id,
+        )
+
+        return pipeline(prompt)
+
+
+zephyr = Zephyr()
+
+answer_relevancy = AnswerRelevancyMetric(
+    threshold=0.7,
+    model=zephyr,
+    include_reason=True
+)
+
+test_cases = costruisci_testcases("dataset_prova.json")
+print(evaluate(
+    test_cases=[test_cases],
+    metrics=[answer_relevancy]
+))
