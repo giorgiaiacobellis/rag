298f2686cb3cd19a973d551a9da38e8adfd6046d
Giorgia Iacobellis <giorgiaiacobellis2000@gmail.com>
2024-10-07 22:02:09 +0200

    Llama-0505

--- data.py
+++ data.py
@@ -140,9 +140,9 @@ config2 = {
 config = {
     "llm": {
             "model": "meta-llama/Meta-Llama-3.1-8B-Instruct",
-            "top_p": 0.2, #testare anche  0.5 , 0.75 , 0.85,  1
+            "top_p": 0.5, #testare anche  0.5 , 0.75 , 0.85,  1
             "max_new_tokens": 1000, #testare anche 250
-            "temperature": 0.2, #testare anche 0.2, 0,5, 0.85, 1
+            "temperature": 0.5, #testare anche 0.2, 0,5, 0.85, 1
             "trust_remote_code":True,  # mandatory for hf models
             "top_k": 10, #testare 1 e 30
             "prompt": utils.get_modified_prompt(
--- langchain_pipeline.py
+++ langchain_pipeline.py
@@ -44,7 +44,7 @@ def generate_db(rag_chain):
     ds  = Dataset.from_dict(dataset_dict["data"])
     #save results 
     
-    filename = "dataset_Llama-0202.json" 
+    filename = "dataset_Llama-0505.json" 
     with open(filename, "w") as outfile:
         json.dump(dataset_dict, outfile) 
     
