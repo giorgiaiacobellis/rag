bdced6a791cdcb669ada7a7f35337a8e6899155c
Giorgia Iacobellis <giorgiaiacobellis2000@gmail.com>
2024-10-09 14:15:59 +0200

    d

--- faithfulness.py
+++ faithfulness.py
@@ -34,7 +34,6 @@ Answer:
 # Template for the LLMChain
 prompt = PromptTemplate(template=prompt_template, input_variables=["context", "statement"])
 
-
 sentence_segmenter= Segmenter(
         language="it",
         clean=False,
@@ -43,6 +42,7 @@ sentence_segmenter= Segmenter(
 
 
 def split_statements(answer):
+    print("split di: ", answer)
     #Splits the generated answer into individual statements based on punctuation and line breaks.
     sentences = sentence_segmenter.segment(answer)
     sentences = [
@@ -64,6 +64,7 @@ def check_statement_relevance(statement, context):
         "<|start_header_id|>assistant<|end_header_id|>\n Risposta: \n\n"
     )
     
+    print("Check statement: " , statement)
     # Query the LLM to get the answer
     result = llm.invoke(prompt)
     
@@ -83,6 +84,7 @@ def calculate_faithfulness_score(context, answer):
         if check_statement_relevance(statement, context):
             relevant_statements += 1
     
+    print("fine calcolo")
     # Faithfulness score is the ratio of relevant statements to total statements
     faithfulness_score = relevant_statements / total_statements
     return faithfulness_score
@@ -94,13 +96,14 @@ if __name__ == "__main__":
     with open("dataset_prova.json", 'r', encoding='utf-8') as f:
         data = json.load(f)
     
-    results = []
-    total_sim = 0
+'''
     for i, item in enumerate(data['data']['question']):
         original_question = item
         answer = data['data']['answer'][i]
-        context = data['data']['contexts'][0][i]
-        
-    # Calculate the faithfulness score
-    score = calculate_faithfulness_score(context, answer)
-    print(f"Faithfulness score: {score:.2f}")
+        context = data['data']['contexts'][1][i]
+'''
+answer = "Il Piemonte \u00e8 conosciuto per la sua cucina tradizionale ricca di sapori e profumi. Tra i piatti tipici piemontesi che dovresti assolutamente provare, ci sono:\n\n1. Agnolotti del Plin: questi piccoli ravioli riempiti di carne, formaggio e spezie sono un classico della cucina piemontese.\n\n2. Vitello Tonnato: questo piatto, composto da insalata di carne di vitello affettata e condimentata con una salsa di tonno, olio d'oliva, aceto balsamico e peperoncino, \u00e8 uno dei piatti pi\u00f9 famosi della regione.\n\n3. Bagna Cauda: questo piatto consiste in una ciotola di salsa di olio, acciughe, aglio e peperoncino, utilizzata per bollire e condire vari tipi di verdure crudite.\n\n4. Brasato al Barolo: il Brasato al Barolo \u00e8 un piatto tradizionale, preparato con una carne di manzo affettata, condita con Barolo e servita con patate e verdure.\n\n5. Torta di Nocciole: questa torta dolce \u00e8 preparata con nocciole, zucchero, uova e sciroppo di mandarino.\n\n6. Polenta: la Polenta \u00e8 una pietanza tipica della cucina piemontese, preparata con farina di mais, acqua e burro.\n\n7. Riso al Barolo: il Riso al Barolo \u00e8 una pietanza preparata con riso, Barolo, carne di manzo, verdure e peperoncino.\n\n8. Gelato: il Gelato \u00e8 un dolce tradizionale, preparato con zucchero, latte, crema e aromi vari, come cioccolato, nocciole, mandarino e fragola.\n\n9. Bicerin: il Bicerin \u00e8 un drink caldo, preparato con espresso, cioccolato e crema di latte.\n\n10. Amaretti di Mand"
+context = "Il Piemonte \u00e8 conosciuto per la sua cucina tradizionale ricca di sapori e profumi. Tra i piatti tipici piemontesi che dovresti assolutamente provare, ci sono:\n\n1. Agnolotti del Plin: questi piccoli ravioli riempiti di carne, formaggio e spezie sono un classico della cucina piemontese.\n\n2. Vitello Tonnato: questo piatto, composto da insalata di carne di vitello affettata e condimentata con una salsa di tonno, olio d'oliva, aceto balsamico e peperoncino, \u00e8 uno dei piatti pi\u00f9 famosi della regione.\n\n3. Bagna Cauda: questo piatto consiste in una ciotola di salsa di olio, acciughe, aglio e peperoncino, utilizzata per bollire e condire vari tipi di verdure crudite.\n\n4. Brasato al Barolo: il Brasato al Barolo \u00e8 un piatto tradizionale, preparato con una carne di manzo affettata, condita con Barolo e servita con patate e verdure.\n\n5. Torta di Nocciole: questa torta dolce \u00e8 preparata con nocciole, zucchero, uova e sciroppo di mandarino.\n\n6. Polenta: la Polenta \u00e8 una pietanza tipica della cucina piemontese, preparata con farina di mais, acqua e burro.\n\n7. Riso al Barolo: il Riso al Barolo \u00e8 una pietanza preparata con riso, Barolo, carne di manzo, verdure e peperoncino.\n\n8. Gelato: il Gelato \u00e8 un dolce tradizionale, preparato con zucchero, latte, crema e aromi vari, come cioccolato, nocciole, mandarino e fragola.\n\n9. Bicerin: il Bicerin \u00e8 un drink caldo, preparato con espresso, cioccolato e crema di latte.\n\n10. Amaretti di Mand"
+# Calculate the faithfulness score
+score = calculate_faithfulness_score(context, answer)
+print(f"Faithfulness score: {score:.2f}")
