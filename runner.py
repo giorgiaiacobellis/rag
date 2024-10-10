10dc60850646738bdcb6af72433f0eeddd53fcaf
Giorgia Iacobellis <giorgiaiacobellis2000@gmail.com>
2024-10-11 00:12:31 +0200

    d

--- runner.py
+++ runner.py
@@ -1,34 +1,5 @@
 import subprocess
 import sys
-
-'''def main():
-    if len(sys.argv) < 2:
-        print("Uso: python runner.py <valore_variabile>")
-        sys.exit(1)
-
-    var = sys.argv[1]
-
-    # Eseguire file1.py
-    subprocess.run(['python3', 'langchain_pipeline.py', var])
-
-    # Eseguire file1.py
-    subprocess.run(['python3', 'answer_relevance.py', var])
-
-    # Eseguire file2.py
-    subprocess.run(['python3', 'faithfulness.py', var])
-
-    # Eseguire file3.py
-    subprocess.run(['python3', 'answer_similarity.py', var])
-
-        # Eseguire file3.py
-    subprocess.run(['python3', 'answer_correctness.py', var])
-
-        # Eseguire file3.py
-    subprocess.run(['python3', 'context_relevance.py', var])
-
-if __name__ == "__main__":
-    main()
-'''
 import subprocess
 import sys
 
@@ -43,13 +14,13 @@ def main():
 
 
         # Eseguire answer_relevance.py
-        #subprocess.run(['python3', 'answer_relevance.py', var])
+        subprocess.run(['python3', 'answer_relevance.py', var])
 
         # Eseguire faithfulness.py
-        #subprocess.run(['python3', 'faithfulness.py', var])
+        subprocess.run(['python3', 'faithfulness.py', var])
 
         # Eseguire answer_similarity.py
-        #subprocess.run(['python3', 'answer_similarity.py', var])
+        subprocess.run(['python3', 'answer_similarity.py', var])
 
         # Eseguire answer_correctness.py
         subprocess.run(['python3', 'answer_correctness.py', var])
@@ -59,3 +30,9 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+'''
+ python3 runner.py dataset_2024-10-07_21-24-15.json dataset_llama_0505_stella.json dataset_2024-10-07_21-32-02.json dataset_llama_0808_stella.json dataset_Llama-0202.json dataset_llama_11_stella.json mist05mp.json dataset_Llama-0505.json dataset_mistral_0202_stella.json mistral_103_stella.json dataset_Llama-0808.json dataset_mistral_0208_stella.json mist03mp.json dataset_Llama-11.json dataset_mistral_0505_stella.json dataset_Mistral-11.json dataset_mistral_0808_stella.json dataset_mistral_11.json dataset_2024-09-27_18-23-43.json dataset_zephyr_0202_stella.json results_2024-09-27_18-26-12.json dataset_2024-10-01_16-00-48.json dataset_gemma_0202_mpnet.json dataset_zephyr_0208_stella.json dataset_2024-10-05_20-02-33.json dataset_gemma_0202_stella.json dataset_zephyr_0505_stella.json dataset_2024-10-06_11-42-12.json dataset_gemma_0208_mpnet.json dataset_zephyr_0808_stella.json dataset_2024-10-06_18-04-50.json dataset_gemma_0208_stella.json dataset_zephyr_11_stella.json dataset_2024-10-06_19-21-42.json dataset_gemma_0505_mpnet.json dataset_2024-10-06_19-50-49.json dataset_gemma_0505_stella.json dataset_2024-10-06_20-06-46.json dataset_gemma_0808.json gemma03mp.json zep03mp.json gemma03stella.json zep03stella.json zep05mp.json llama03mp.json zep05stella.json llama03stella.json llama05mp.json
+ 
+ '''   
