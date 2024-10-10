f74fd8382fb5383b6425f2ea76c51a5ccaab989c
Giorgia Iacobellis <giorgiaiacobellis2000@gmail.com>
2024-10-10 20:50:29 +0200

    d

--- runner.py
+++ runner.py
@@ -1,7 +1,7 @@
 import subprocess
 import sys
 
-def main():
+'''def main():
     if len(sys.argv) < 2:
         print("Uso: python runner.py <valore_variabile>")
         sys.exit(1)
@@ -19,6 +19,43 @@ def main():
 
     # Eseguire file3.py
     subprocess.run(['python3', 'answer_similarity.py', var])
+
+        # Eseguire file3.py
+    subprocess.run(['python3', 'answer_correctness.py', var])
+
+        # Eseguire file3.py
+    subprocess.run(['python3', 'context_relevance.py', var])
+
+if __name__ == "__main__":
+    main()
+'''
+import subprocess
+import sys
+
+def main():
+    if len(sys.argv) < 2:
+        print("Uso: python runner.py <file1> <file2> ... <fileN>")
+        sys.exit(1)
+
+    # Itera attraverso tutti i file passati come argomenti
+    for var in sys.argv[1:]:
+        print(f"Eseguendo script sui dati del file: {var}")
+
+
+        # Eseguire answer_relevance.py
+        #subprocess.run(['python3', 'answer_relevance.py', var])
+
+        # Eseguire faithfulness.py
+        #subprocess.run(['python3', 'faithfulness.py', var])
+
+        # Eseguire answer_similarity.py
+        #subprocess.run(['python3', 'answer_similarity.py', var])
+
+        # Eseguire answer_correctness.py
+        subprocess.run(['python3', 'answer_correctness.py', var])
+
+        # Eseguire context_relevance.py
+        subprocess.run(['python3', 'context_relevance.py', var])
 
 if __name__ == "__main__":
     main()
